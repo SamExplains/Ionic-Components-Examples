5e1ed9b55bc3073ebf0899cb947296f0d988a188
Sampuccino <Sampuccino@gmail.com>
2018-10-24 21:34:03 -0700

    More in depth look at ionic components and properties.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, reorderArray } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,12 +7,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private items = ['French Bulldog', 'English Bulldog', 'American Bulldog'];
+
+  constructor(public navCtrl: NavController ) {
 
   }
 
   onClick(){
     console.log('Clicked');
+  }
+
+  reorderListItems(index){
+    this.items = reorderArray(this.items, index);
   }
 
 }
